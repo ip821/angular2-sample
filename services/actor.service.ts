f0781c80e37a9c03cb43b857cdcd448d40685a01
igor <igor>
2016-03-11 11:01:01 +0200

    fix

--- services/actor.service.ts
+++ services/actor.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "angular2/core";
 import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
 
 export interface IActor {
     firstName: string;
@@ -16,11 +17,16 @@ export class ActorService {
 
     }
 
-    getActors() {
+    getActors(): Observable<IActor[]> {
+        
+        if (ActorService._actors != null) {
+            return Observable.of(ActorService._actors);
+        }
+
         return this._http
             .get("data/actors.json")
             .map(res => {
-                if(ActorService._actors == null){
+                if (ActorService._actors == null) {
                     ActorService._actors = <IActor[]>res.json();
                 }
                 return ActorService._actors;
